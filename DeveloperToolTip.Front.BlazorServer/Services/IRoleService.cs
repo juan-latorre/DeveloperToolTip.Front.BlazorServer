281a91d21f49673f1bac7008a38e12f1f7d4f510
juan-latorre <juanlatorrevidal@gmail.com>
2025-01-14 14:17:31 -0500

    Se modifica el parametro

--- DeveloperToolTip.Front.BlazorServer/Services/IRoleService.cs
+++ DeveloperToolTip.Front.BlazorServer/Services/IRoleService.cs
@@ -5,7 +5,7 @@ namespace DeveloperToolTip.Front.BlazorServer.Services
     public interface IRoleService
     {
         Task<IEnumerable<RoleDto>> GetAllRoles();
-        Task<RoleDto?> GetRoleById(int id);
+        Task<RoleDto?> GetRoleById(int roleId);
         Task<bool> CreateRole(CreateRoleDto role);
         Task<bool> UpdateRole(RoleDto role);
         Task<bool> DeleteRole(int roleId);
