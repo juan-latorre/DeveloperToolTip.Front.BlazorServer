1972aa01060b078dc24a51ad5036ef43078aaae9
juan-latorre <juanlatorrevidal@gmail.com>
2025-01-17 09:58:03 -0500

    Se implementa el servicio de Google News para consumirlo en el Home

---
+++ DeveloperToolTip.Front.BlazorServer/Components/Pages/BannerGoogleNews.razor.cs
@@ -0,0 +1,17 @@
+﻿using DeveloperToolTip.Front.BlazorServer.Models;
+using Microsoft.AspNetCore.Components;
+using Radzen;
+
+namespace DeveloperToolTip.Front.BlazorServer.Components.Pages
+{
+    public partial class BannerGoogleNews : ComponentBase
+    {
+        private IEnumerable<GoogleNewsDto>? news;
+
+        protected override async Task OnInitializedAsync()
+        {
+            news = await _IGoogleNewsService.GetNewsAsync("technology");
+        }
+
+    }
+}
