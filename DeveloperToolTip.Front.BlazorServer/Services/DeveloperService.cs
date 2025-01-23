b51f4505e0bf0f3b90a703ed1b5dc642aae33000
juan-latorre <juanlatorrevidal@gmail.com>
2025-01-23 17:48:53 -0500

    Se implementa DeveloperServices y ajuste de Header

---
+++ DeveloperToolTip.Front.BlazorServer/Services/DeveloperService.cs
@@ -0,0 +1,32 @@
+﻿using DeveloperToolTip.Front.BlazorServer.Models;
+
+namespace DeveloperToolTip.Front.BlazorServer.Services
+{
+    public class DeveloperService : IDeveloperService
+    {
+        private readonly HttpClient _httpClient;
+
+        public DeveloperService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        // Developers: Get All Developers
+        public async Task<IEnumerable<DeveloperDto>> GetAllRoles()
+        {
+            var response = await _httpClient.GetFromJsonAsync<IEnumerable<DeveloperDto>>("Developers");
+
+            return response ?? Enumerable.Empty<DeveloperDto>();
+        }
+
+        //Get Developer By Id
+        public async Task<DeveloperDto?> GetDevelopersById(int DeveloperId)
+        {
+            var response = await _httpClient.GetFromJsonAsync<DeveloperDto>($"Developers/{DeveloperId}");
+            return response;
+        }
+
+
+
+    }
+}
---
+++ DeveloperToolTip.Front.BlazorServer/Services/IDeveloperService.cs
@@ -0,0 +1,10 @@
+﻿using DeveloperToolTip.Front.BlazorServer.Models;
+
+namespace DeveloperToolTip.Front.BlazorServer.Services
+{
+    public interface IDeveloperService
+    {
+        Task<IEnumerable<DeveloperDto>> GetAllRoles();
+        Task<DeveloperDto?> GetDevelopersById(int DeveloperId);
+    }
+}
